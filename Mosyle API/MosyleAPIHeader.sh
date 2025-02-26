c5539a4322d2bd1c9f8c2a8ba4ff812cf15561b6
Cory Funk <fud18@users.noreply.github.com>
2025-02-26 16:27:16 -0600

    Create MosyleAPIHeader.sh

---
+++ Mosyle API/MosyleAPIHeader.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ===================================================================================
+# Add to the start of any script that needs to make an API call 
+# ===================================================================================
+
+# Define the Access Token for authentication with Mosyle API
+accessToken="<ACCESS TOKEN>"
+bearer_token="%custom_bearer_token_1%\
+%custom_bearer_token_2%\
+%custom_bearer_token_3%\
+%custom_bearer_token_4%\
+%custom_bearer_token_5%\
+%custom_bearer_token_6%"
+
+# End of script
